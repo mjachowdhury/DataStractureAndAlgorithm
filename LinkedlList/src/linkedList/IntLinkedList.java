0e8ce0bc32da0652075de1a4ca9419eb0abd0fd4
mjachowdhury <study.computing2016@gmail.com>
2017-09-17 01:23:58 +0100

    Descending order checking
    with the help of linkedlist bubble sort algorithm used

--- LinkedlList/src/linkedList/IntLinkedList.java
+++ LinkedlList/src/linkedList/IntLinkedList.java
@@ -1,71 +1,112 @@
 package linkedList;
 
 public class IntLinkedList {
-	//field of the class its global
+	// field of the class its global
 	private Node head;
-	
-	public IntLinkedList(int item){
-		head = new Node();//instantiate the Node 
-		head.value = item;//this is the first and last node
+
+	// changing parameter cause when we will work with object then object will
+	// not work with primitive
+	public IntLinkedList() {
+		head = new Node();// instantiate the Node
+		head.value = 0;// this is the first and last node
 		head.link = null;
 	}
-	
+
 	/*
 	 * This function will adding the item inside the linked list
 	 */
-	public boolean insertItem(int item){
-		Node n = new Node();//creating new node
-		n.value = item;  
-		n.link = head; 
-		head = n;
+	public boolean insertItem(int item) {
+		Node n = new Node();// creating new node
+		/*
+		 * n.value = item; n.link = head; head = n; return true;
+		 */
+
+		// to add the item in the last
+
+		Node new_node;
+		new_node = head;// take the address from the head
+		// once we get the address of hte first node then we need to iterate
+		// until the end
+		while (new_node.link != null) {
+			new_node = new_node.link;
+		}
+		n.value = item;
+		n.link = null;
+		new_node.link = n;
 		return true;
 	}
-	
+
 	/*
-	 * This function will show all the item inside the linked list 
+	 * This function will show all the item inside the linked list
 	 */
-	public void ShowAllNode(){
-		//first get the address of the first node from the head
-		Node z = head;
-		while(z!=null){ //looping until its not null
+	public void ShowAllNode() {
+		// first get the address of the first node from the head
+		Node z = head.link;// it will call the second node
+		while (z != null) { // looping until its not null
 			System.out.println(z.value);
 			z = z.link;
 		}
 	}
-	
+
 	/*
 	 * This function will delete the item from the linked list
 	 */
-	public boolean deleteItem(int item){
-		if(head.value == item){//if we find the item in the first head then
-			head = head.link;//means it will go the fist node and will take to the first link get the address of the second node
+	public boolean deleteItem(int item) {
+		if (head.link.value == item) {// if we find the item in the first head
+										// then
+			head.link = head.link.link;// means it will go the fist node and
+										// will take to the first link get the
+										// address of the second node
 			return true;
-		} else{//if you don't find it then we have to iterate the whole list
-			Node x = head;//getting the address first
+		} else {// if you don't find it then we have to iterate the whole list
+			Node x = head;// getting the address first
 			Node y = head.link;
-			while(true){
-				if(y == null || y.value == item){
-					break;//y will be null when it will reach to the last.that means we havan't found any item in the list
-				} else{
-					x = y;//x will store the address of the y 
-					y= y.link;//here y will store the next node address until we find the item
-					
+			while (true) {
+				if (y == null || y.value == item) {
+					break;// y will be null when it will reach to the last.that
+							// means we havan't found any item in the list
+				} else {
+					x = y;// x will store the address of the y
+					y = y.link;// here y will store the next node address until
+								// we find the item
+
 				}
 			}
-			if(y != null){//that means we found the item
+			if (y != null) {// that means we found the item
 				x.link = y.link;
 				return true;
-			}else{
+			} else {
 				return false;
 			}
 		}
-		
+
 	}
-	//this nested classes
-	//this Node will work within this LinkedList class
-	
-	//child class of parent class of IntLinkedList
-	class Node{
+
+	public void sortList() {
+		int c = 0;
+		Node a = head.link;
+		while (a.link != null) { // bubble sort algorithm with the help of
+									// linked list
+			Node b = head.link;
+			while (b.link != null) {
+				if (b.value < b.link.value) {// < its means we are sorting
+												// descending order.if we want
+												// ascending order then we need
+												// to put > sign
+					c = b.value;
+					b.value = b.link.value;
+					b.link.value = c;
+				}
+				b = b.link;
+			}
+			a = a.link;
+		}
+	}
+	// this nested classes
+	// this Node will work within this LinkedList class
+
+	// child class of parent class of IntLinkedList
+	class Node {
 		private int value;
 		private Node link;
 	}
--- LinkedlList/src/linkedList/MainLinkedList.java
+++ LinkedlList/src/linkedList/MainLinkedList.java
@@ -1,20 +1,26 @@
 package linkedList;
 
 public class MainLinkedList {
-	public static void main(String [] args){
-		IntLinkedList list = new IntLinkedList(2);
-		list.insertItem(5);
+	public static void main(String[] args) {
+		IntLinkedList list = new IntLinkedList();
+
 		list.insertItem(4);
 		list.insertItem(78);
 		list.insertItem(89);
-		
-		list.ShowAllNode();//it will print reverse list
-		//anything we entered it be in the lst
-		
+		list.insertItem(5);
+
+		list.ShowAllNode();// it will print reverse list
+		// anything we entered it be in the lst
+
 		System.out.println();
-		
-		list.deleteItem(5);
-		list.ShowAllNode();
+
+		// list.deleteItem(5);
+		// list.ShowAllNode();
+
+		// calling the sort list function
+		System.out.println();
+		list.sortList();
+		list.ShowAllNode();// will print the descending order
 	}
 
 }
